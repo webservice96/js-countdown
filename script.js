cc2c1639e366f22642d366f6ffa178feda72f6ce
Helal Uddin <dmdhelal@gmail.com>
2021-07-22 23:24:23 +0600

    Countdown Project

---
+++ script.js
@@ -0,0 +1,30 @@
+const daysEl = document.querySelector('#days');
+const hoursEl = document.querySelector('#hours');
+const minsEl = document.querySelector('#mins');
+const secondsEl = document.querySelector('#seconds');
+const newYears = "1 Jan 2022";
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// init call
+countdown();
+setInterval(countdown, 1000);
